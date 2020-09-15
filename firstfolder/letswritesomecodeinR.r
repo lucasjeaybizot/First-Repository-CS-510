32fc8b0c4617041c1ff597b9d204dfd3bcdc2891
root <jeaybizot@chapman.edu>
2020-09-15 14:23:52 +0200

    Hello world but in R

---
+++ firstfolder/letswritesomecodeinR.r
@@ -0,0 +1,11 @@
+#Name: Lucas Jeay-Bizot
+#Class: CS 510
+#University: Chapman University
+#Student ID: 2376319
+#Date Created: 15/09/2020
+#Date Last Modified: 15/09/2020 (14:22)
+#Assignment 3 - create a git repository with at least 3 commits (this time in R)
+
+print("Hello World", quote = FALSE)
+
+
